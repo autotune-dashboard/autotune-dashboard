3f717d032b3a8fe4b51baf66709dbaea668bc130
Evgenii Khramkov <ewgeniux@gmail.com>
2018-08-06 22:01:04 +0300

    base layout for auth screen

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  // palette: {
+  //   primary: {
+  //     main: '#311b92',
+  //   },
+  //   secondary: {
+  //     main: '#0097a7',
+  //   }
+  // }
+});
--- src/views/auth.tsx
+++ src/views/auth.tsx
@@ -3,16 +3,30 @@ import { RouteComponentProps } from 'react-router-dom';
 import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 const styles = (({ palette, spacing }: Theme) => createStyles({
   root: {
     flexGrow: 1
   },
   left: {
-    backgroundColor: palette.primary.dark
+    display: 'flex',
+    flexFlow: 'column'
   },
   right: {
     backgroundColor: palette.secondary.dark
+  },
+  wrapper: {
+    flexGrow: 1
+  },
+  container: {
+    maxWidth: 320
+  },
+  formItem: {
+    marginBottom: spacing.unit * 2,
+    width: '100%'
   }
 }));
 
@@ -24,13 +38,20 @@ export const Auth = withStyles(styles)(
     public render() {
       const { classes } = this.props;
       return (
-        <Grid className={classes.root} container spacing={16}>
+        <Grid className={classes.root} container>
           <Grid className={classes.left} item xs={12} md={6}>
-            1
+            <Grid className={classes.wrapper} container justify='center' alignItems='center' direction='column'>
+              <Grid className={classes.container} container justify='space-around' alignItems='flex-start' direction='column'>
+                <Typography variant="display1" gutterBottom>
+                  Login
+                </Typography>
+                <TextField className={classes.formItem} label='email' />
+                <TextField className={classes.formItem} label='password' />
+                <Button variant='contained' color='primary'>Login</Button>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid className={classes.right} item xs={12} md={6}>
-            2
-          </Grid>
+          <Grid className={classes.right} item xs={12} md={6} />
         </Grid>
       );
     }
